c86d4e2387988499f8e042714e109e54de094f6a
Ch3mZ-ZA <ch3mzza@gmail.com>
2023-11-21 20:43:47 +0200

    ZN-004 Terminal Interface

--- main.go
+++ main.go
@@ -4,114 +4,14 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/charmbracelet/bubbles/textinput"
+	"github.com/ch3mz-za/znox/iface"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
 )
 
-type Styles struct {
-	BorderColor lipgloss.Color
-	InputField  lipgloss.Style
-}
-
-type Questions struct {
-	question string
-	answer   string
-}
-
-func NewQuestion(question string) Questions {
-	return Questions{question: question}
-}
-
-func DefaultStyles() *Styles {
-	s := new(Styles)
-	s.BorderColor = lipgloss.Color("36")
-	s.InputField = lipgloss.NewStyle().BorderForeground(s.BorderColor).BorderStyle(lipgloss.NormalBorder()).Padding(1).Width(80)
-	return s
-}
-
-type model struct {
-	width       int
-	height      int
-	index       int
-	styles      *Styles
-	answerField textinput.Model
-	questions   []Questions
-}
-
-func New(questions []Questions) *model {
-	styles := DefaultStyles()
-	answerField := textinput.New()
-	answerField.Placeholder = "Your answer here"
-	answerField.Focus()
-	return &model{
-		questions:   questions,
-		answerField: answerField,
-		styles:      styles,
-	}
-}
-
-func (m *model) Init() tea.Cmd {
-	return nil
-}
-
-func (m *model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var cmd tea.Cmd
-	current := &m.questions[m.index]
-
-	switch msg := msg.(type) {
-
-	case tea.WindowSizeMsg:
-		m.width = msg.Width
-		m.height = msg.Height
-
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "ctrl+c":
-			return m, tea.Quit
-		case "enter":
-			current.answer = m.answerField.Value()
-			m.answerField.SetValue("")
-			m.Next()
-			return m, nil
-		}
-	}
-	m.answerField, cmd = m.answerField.Update(msg)
-	return m, cmd
-}
-
-func (m *model) View() string {
-	if m.width == 0 {
-		return "loading..."
-	}
-	return lipgloss.Place(
-		m.width,
-		m.height,
-		lipgloss.Center,
-		lipgloss.Center,
-		lipgloss.JoinVertical(
-			lipgloss.Center,
-			m.questions[m.index].question,
-			m.styles.InputField.Render(m.answerField.View()),
-		),
-	)
-}
-
-func (m *model) Next() {
-	if m.index < len(m.questions)-1 {
-		m.index++
-	} else {
-		m.index = 0
-	}
-}
-
 func main() {
-	questions := []Questions{
-		NewQuestion("Eet jy kaas?"),
-		NewQuestion("Eet jy grond?"),
-		NewQuestion("Eet jy gras?"),
+	prompts := []iface.Prompt{
+		iface.NewPrompt("Enter password:"),
 	}
-	m := New(questions)
 
 	f, err := tea.LogToFile("debug.log", "debug")
 	if err != nil {
@@ -119,7 +19,7 @@ func main() {
 	}
 	defer f.Close()
 
-	p := tea.NewProgram(m, tea.WithAltScreen())
+	p := tea.NewProgram(iface.New(prompts), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		log.Fatal(err)
 	}
